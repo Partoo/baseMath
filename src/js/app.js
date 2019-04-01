85305b6e9d5ebb9f295c0d8e20538f5bf3e78251
Partoo Hwong <partoo@163.com>
2019-04-01 11:53:44 +0800

    remove cdn

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+var $ = require('jquery')
 var Vue = require('vue')
 var App = require('./app.vue')
 var sign = require('./sign.js');
--- src/js/app.vue
+++ src/js/app.vue
@@ -14,8 +14,13 @@
    <span class="card orange" id="{{answer}}" v-on:click="isCorrect($event)" v-for="answer in answers">{{answer}}</span>
  </div>  
 </div>
-<div id="cloudy">
-
+<div class="background">
+   <svg class="clouds cloud1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0" y="0" width="512" height="512" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
+    <path id="cloud-icon" d="M406.1 227.63c-8.23-103.65-144.71-137.8-200.49-49.05 -36.18-20.46-82.33 3.61-85.22 45.9C80.73 229.34 50 263.12 50 304.1c0 44.32 35.93 80.25 80.25 80.25h251.51c44.32 0 80.25-35.93 80.25-80.25C462 268.28 438.52 237.94 406.1 227.63z"/>
+  </svg>
+  <svg class="clouds cloud2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0" y="0" width="512" height="512" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
+    <path id="cloud-icon" d="M406.1 227.63c-8.23-103.65-144.71-137.8-200.49-49.05 -36.18-20.46-82.33 3.61-85.22 45.9C80.73 229.34 50 263.12 50 304.1c0 44.32 35.93 80.25 80.25 80.25h251.51c44.32 0 80.25-35.93 80.25-80.25C462 268.28 438.52 237.94 406.1 227.63z"/>
+  </svg>
 </div>
 <footer>                
   <span>机会:{{life}}</span>
@@ -67,9 +72,9 @@
       gameOver:function(){
         $('.overlay').show();
         if (this.score>=100) {
-          this.comment = '宝贝全对啦,但要注意保护视力喔'
+          this.comment = '全做对啦,但要注意保护视力喔'
         }else{
-          this.comment = '宝贝得了' + this.score + '分,加油!'
+          this.comment = '得了' + this.score + '分,加油喔!'
         };
       },
       getType:function(){
